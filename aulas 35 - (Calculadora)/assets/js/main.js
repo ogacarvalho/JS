cc3dd533d7ab7a80e63d207321a137b35bc1305b
Gabriel <arrobacarvalho@gmail.com>
2021-11-20 08:37:31 -0300

    Novas Aulas

---
+++ aulas 35 - (Calculadora)/assets/js/main.js
@@ -0,0 +1,37 @@
+
+(() => {
+    
+    calculadora = {
+        input: document.querySelector('.input'),                                           
+
+        inicia(){                                                                           
+
+        document.addEventListener('click', (e) => {                                      // Tracking do Click
+            
+            let elemento = e.target;   
+            let classe = (nome) => elemento.classList.contains(nome);
+            let display = (input) => this.input.value += input;
+            let clear = () => this.input.value = ''; 
+            let del = () => this.input.value = this.input.value.slice(0, -1);           // mostra input (menos) último caráctere
+            let enter = () => this.input.value = eval(this.input.value);                // Executa o que estiver dentro.
+                                    
+                if(classe('btn__num')) {display(elemento.innerText)}
+                if(classe('btn__cle')) {clear()}
+                if(classe('btn__del')) {del()}
+                if(classe('btn__equ')) {this.input.value? enter():alert('Digite algo.')}
+
+        })  
+
+        },
+
+    }
+    calculadora.inicia();
+
+})();
+
+/* DICAS
+1. Sempre que precisar referenciar um objeto de dentro de uma chave de um objeto é necessário o uso do (This)
+2. Em objetos, se você criar um elemento/objeto e incluir nele funções (métodos) o this muda de pai, a menos que use arrow. (bind(this))
+3. Checar erro? Valide o eval() || Executar ao clicar no enter? Evento + KeyCode === 13
+*/
+
