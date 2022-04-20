10eb26b3d3ca02d22ea6b00aa8fce1b74f3cc3af
Gabriel C <arrobacarvalho@gmail.com>
2022-04-20 08:44:15 -0300

    SOLID

---
+++ TS/aulas/basico/src/namespace.ts
@@ -0,0 +1,59 @@
+
+// Namespace: 
+// → É uma ferramenta de criação de escopo, muito semelhante ao modules.
+// → É possível criar outros namespaces dentro do mesmo.
+// → É recomendado usar apenas o modules, embora tenhamos este recurso.
+
+namespace Teste {
+    const exemplo_0 = 'Gabriel';
+    export const exemplo_1 = 'Maria';
+}
+
+console.log(Teste.exemplo_1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default 1;
