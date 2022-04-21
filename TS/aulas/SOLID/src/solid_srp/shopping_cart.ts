f20044c78daacfa187427803f71f1f5f7290107b
G <80264098+ogacarvalho@users.noreply.github.com>
2022-04-21 19:02:02 -0300

    Update Shopping Cart

--- TS/aulas/SOLID/src/solid_srp/shopping_cart.ts
+++ TS/aulas/SOLID/src/solid_srp/shopping_cart.ts
@@ -1,48 +1,50 @@
 
-type cart_item = { name: string; price: number };
-type order_status = 'open' | 'closed';
+type cart_item = { name: string; price: number };                                                      // Type para representar a estrutura dos itens do carrinho.
+type order_status = 'open' | 'closed';                                                                 // Type para representação de Flag.
 
+/* Classe Carrinho de Compras:
+    A estrutura irá conter duas propriedades privadas uma que representará o carrinho propriamente dito, e receberá uma lista de itens do type "cart_item", iniciando-se como um array vázio.
+    A segunda propriedade será uma flag indicará se o carrinho está em uso ou não.
 
-class ShoppingCart {
-    // Terá um [] de cart_items e começará como um [].
-    private readonly _items: cart_item[] = [];
-    private _order_status: order_status = 'open';
+    O restante da estrutura, será composta por métodos básicos, e gets para permitir a visualização das propriedades privadas da classe.
+*/
+class ShoppingCart {                
+    private readonly _items: cart_item[] = [];                                                         // Terá um [] de cart_items e começará como um [].
+    private _order_status: order_status = 'open';                                                      // Flag de carrinho, inicialmente aberta.
 
     
-    add_item(item: cart_item): void {
+    add_item(item: cart_item): void {                                                                  // Push do objeto na propriedade privada "_items".
         this._items.push(item);
     }
 
 
-    remove_item(index: number): void {
+    remove_item(index: number): void {                                                                 // Remoção do objeto com splice via [index], enviado no parâmetro do método.
         this._items.splice(index, 1);
     }
 
     // O [+] converte novamente para number, uma vez que o toFixed retorna string.
-    total(): number {
-        return +this._items.reduce((total, current) => total + current.price, 0).toFixed(2);
-        
+    total(): number {                                                                                  // Ferramenta de soma com [reduce] > Acumulador + index[0]['propriedade_do_objeto']
+        return +this._items.reduce((total, current) => total + current['price'], 0).toFixed(2);
     }
 
-
-    is_empty(): boolean {
+    is_empty(): boolean {                                                                              // Método que diz se o carrinho está vázio.
         return this._items.length === 0;
     }
 
-    send_message(msg: string): void {
+    send_message(msg: string): void {                                                                  // Log do argumento.
         return console.log(msg);
     }
 
-    save_order(): void {
+    save_order(): void {                                                                               // "Fake"
         return console.log('Pedido salvo.');
     }
 
-    clear(): void {
+    clear(): void {                                                                                    // Zera o carrinho através do reset do tamanho.
         this._items.length = 0;
         return console.log('Carrinho livre!');
     }
 
-    checkout(): void {
+    checkout(): void {                                                                                 // Método que aciona conjunto de outros métodos para realizar o "Checkout"
         if(this.is_empty()) {
             console.log('Seu carrinho está vázio.');
             return;
@@ -55,28 +57,22 @@ class ShoppingCart {
         return;
     }
 
-    get items(): Readonly<cart_item[]> {
+    get items(): Readonly<cart_item[]> {                                                               // Get para visualização dos valores da propriedade privada [carrinho]
         return this._items;
     }
 
-    get order_status(): Readonly<order_status> {
+    get order_status(): Readonly<order_status> {                                                       // Get para visualizar o estado do carrinho.
         return this._order_status;
     }
 };
 
 const cart = new ShoppingCart;
-const pizza: cart_item = { name: 'Mozzarela', price: 0.9 };
-const soda: cart_item = { name: 'Coke', price: 1.7 };
-const chocolate: cart_item = { name: 'Snickers', price: 0.5 };
-const wine: cart_item = { name: 'Red Wine', price: 12.12 };
+const pizza: cart_item = { name: 'Mozzarela', price: 0.9 };      cart.add_item(pizza);
+const soda: cart_item = { name: 'Coke', price: 1.7 };            cart.add_item(soda);
+const chocolate: cart_item = { name: 'Snickers', price: 0.5 };   cart.add_item(chocolate);
+const wine: cart_item = { name: 'Red Wine', price: 12.12 };      cart.add_item(wine);
 
-cart.add_item(pizza);
-cart.add_item(soda);
-cart.add_item(chocolate);
-cart.add_item(wine);
-
-console.log(cart);
-cart.checkout();
+console.log(cart); cart.checkout();
 console.log(cart.order_status);
 
 
