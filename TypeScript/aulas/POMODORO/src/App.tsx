9aa1d01151f5954099ef6ad421ef47f0a92991ad
Gabriel C <arrobacarvalho@gmail.com>
2022-06-23 08:04:03 -0300

    Update

--- TypeScript/aulas/POMODORO/src/App.tsx
+++ TypeScript/aulas/POMODORO/src/App.tsx
@@ -1,16 +1,8 @@
 import React from 'react';
-import { PomodoroTimer } from './components/pomodoro-timer';
 
 function App(): JSX.Element {
   return (
-    <div className="container">
-      <PomodoroTimer
-        pomodoroTime={1500} // 25min
-        shortRestTime={300} // 5min - For each pomodoro one shortRestTime
-        longRestTime={900} // 15min
-        cycles={4} // For each end of cycle one longRestTime
-      />
-    </div>
+    <div className="App">Oi</div>
   );
 }
 
--- TypeScript/aulas/POMODORO/src/utils/zero-left.ts
+++
@@ -1,3 +0,0 @@
-const zeroLeft = (n: number): string =>
-  Math.floor(n).toString().padStart(2, '0');
-export { zeroLeft };
