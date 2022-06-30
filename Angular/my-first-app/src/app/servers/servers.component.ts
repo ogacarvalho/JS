2b0531e44a00b4c042a9f49aa105ce14b9b38cc1
Gabriel C <arrobacarvalho@gmail.com>
2022-06-30 08:13:03 -0300

    Basic Directives

--- Angular/my-first-app/src/app/servers/servers.component.ts
+++ Angular/my-first-app/src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',                  // Podemos separar o arquivo, ou digitar o html diretamente [inline] como abaixo:
@@ -9,13 +10,17 @@ export class ServersComponent implements OnInit {
   public addNewServer:boolean = false;
   public serverCreationStatus:string = 'No server was created!';
   public serverName:string = 'Type the name of your server.';
+  public serverCreated:boolean = false;
+  public servers: string[] = ['Server_0', 'Server_1'];
 
   constructor() { 
     setTimeout(() => { this.addNewServer = true },2000);
   }
 
   onCreateServer():void {
-    this.serverCreationStatus = `Server ${this.serverName} was Created!`;
+      this.servers.push(this.serverName);
+      this.serverCreated = true;
+      this.serverCreationStatus = `A server was Created!`;
   };
 
   ngOnInit(): void {
